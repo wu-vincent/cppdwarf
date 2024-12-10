c51715a92b53e2ba2ea9d711917d925e0939274d
Vincent <magicdroidx@gmail.com>
2024-12-10 04:30:26 +0000

    feat: add argparse library for command-line argument parsing

--- examples/dwarf2cpp/src/main.cpp
+++ examples/dwarf2cpp/src/main.cpp
@@ -1,6 +1,23 @@
+#include <argparse/argparse.hpp>
 #include <cppdwarf/cppdwarf.hpp>
+
+namespace dw = cppdwarf;
 
 int main(int argc, char *argv[])
 {
+    argparse::ArgumentParser parser("cpp2dwarf");
+    parser.add_argument("path").help("path to a DWARF debug symbol file");
+    try {
+        parser.parse_args(argc, argv);
+    }
+    catch (std::exception &err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+    auto path = parser.get<std::string>("path");
+    auto debug = dw::debug(path);
+
     return 0;
 }
