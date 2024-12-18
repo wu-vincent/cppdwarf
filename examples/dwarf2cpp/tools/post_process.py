6131c0027ce9e4df57bf642f319f19f9886f4b73
Vincent <magicdroidx@gmail.com>
2024-12-18 18:28:29 +0000

    feat: improve post-processing steps

--- examples/dwarf2cpp/tools/post_process.py
+++ examples/dwarf2cpp/tools/post_process.py
@@ -35,40 +35,59 @@ def process_file(args):
         "std::string",
     )
     content = content.replace(
+        "std::basic_string_view<char, std::char_traits<char> >",
+        "std::string_view",
+    )
+    content = content.replace(
         "std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<long long, std::ratio<1L, 1000000000L> > >",
         "std::chrono::steady_clock::time_point",
     )
+    content = re.sub(r"\(lambda at .+?\)", "Lambda", content)
 
-    # Regex to match and replace complex types in std::unique_ptr with default_delete
-    pattern = r"std::unique_ptr<(.+?),\s*std::default_delete<\1\s*>\s*>"
-    content = recursive_sub(pattern, r"std::unique_ptr<\1>", content)
+    pattern_repl = [
+        # std::unique_ptr
+        (r"std::unique_ptr<(.+?),\s*std::default_delete<\1\s*>\s*>", r"std::unique_ptr<\1>"),
+        # std::vector
+        (r"std::vector<(.+?),\s*std::allocator<\1\s*>\s*>", r"std::vector<\1>"),
+        # std::list
+        (r"std::list<(.+?),\s*std::allocator<\1\s*>\s*>", r"std::list<\1>"),
+        # std::deque
+        (r"std::deque<(.+?),\s*std::allocator<\1\s*>\s*>", r"std::deque<\1>"),
+        # std::queue
+        (r"std::queue<(.+?),\s*std::deque<\1\s*>\s*>", r"std::queue<\1>"),
+        # std::unordered_map
+        (r"std::unordered_map<(.+?),\s*(.+?),\s*"
+         r"std::hash<\1\s*>,\s*"
+         r"std::equal_to<\1\s*>,\s*"
+         r"std::allocator<std::pair<const \1,\s*\2\s*>\s*>\s*>", r"std::unordered_map<\1, \2>"),
+        # std::unordered_set
+        (r"std::unordered_set<(.+?),\s*std::hash<\1\s*>,\s*"
+         r"std::equal_to<\1\s*>,\s*std::allocator<\1\s*>\s*>", r"std::unordered_set<\1>"),
+        # std::map
+        (r"std::map<(.+?),\s*(.+?),\s*std::less<\1\s*>,\s*"
+         r"std::allocator<std::pair<const \1,\s*\2\s*>\s*>\s*>", r"std::map<\1, \2>"),
+        # std::set
+        (r"std::set<(.+?),\s*std::less<\1\s*>,\s*std::allocator<\1\s*>\s*>", r"std::set<\1>"),
+        # gsl::span
+        (r"gsl::span<(.+),\s*\d+UL>", r"gsl::span<\1>"),
+        # glm::vec
+        (r"glm::vec<(\d),\s*float,\s*\(glm::qualifier\)0>", r"glm::vec\1"),
+        # glm::mat
+        (r"glm::mat<(\d),\s*(\d),\s*float,\s*\(glm::qualifier\)0>", r"glm::mat\1x\2"),
+        # duplicated anonymous types
+        (r"((?:union|enum|struct|class)\s*{[\s\S]+?});\s*(\1\s.+)", r"\2"),
+        (r"((?:union|enum|struct|class)\s*{[\s\S]+?})(.+)(\s*\1;)", r"\1\2"),
+    ]
 
-    # Regex to match and replace std::vector with default allocator
-    pattern = r"std::vector<(.+?),\s*std::allocator<\1\s*>\s*>"
-    content = recursive_sub(pattern, r"std::vector<\1>", content)
+    while True:
+        output = content
+        for pattern, repl in pattern_repl:
+            output = re.sub(pattern, repl, output)
 
-    # Regex for std::unordered_map with complex default parameters
-    pattern = (
-        r"std::unordered_map<(.+?),\s*(.+?),\s*"
-        r"std::hash<\1\s*>,\s*"
-        r"std::equal_to<\1\s*>,\s*"
-        r"std::allocator<std::pair<const \1,\s*\2\s*>\s*>\s*>"
-    )
-    content = recursive_sub(pattern, r"std::unordered_map<\1, \2>", content)
+        if output == content:
+            break
 
-    # Regex for std::map with complex default parameters
-    pattern = (
-        r"std::map<(.+?),\s*(.+?),\s*std::less<\1\s*>,\s*"
-        r"std::allocator<std::pair<const \1,\s*\2\s*>\s*>\s*>"
-    )
-    content = recursive_sub(pattern, r"std::map<\1, \2>", content)
-
-    # Regex for std::unordered_set with complex default parameters
-    pattern = (
-        r"std::unordered_set<(.+?),\s*std::hash<\1\s*>,\s*"
-        r"std::equal_to<\1\s*>,\s*std::allocator<\1\s*>\s*>"
-    )
-    content = recursive_sub(pattern, r"std::unordered_set<\1>", content)
+        content = output
 
     # Calculate relative path
     relative_path = file_path.relative_to(input_path)
